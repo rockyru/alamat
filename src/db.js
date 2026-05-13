4837d8fc29561ddc92515cf46bcaeeec35904467
rmbergonia <rmbergonia1@up.edu.ph>
2026-05-13 17:45:21 +0800

    added more features

--- src/db.js
+++ src/db.js
@@ -1,12 +1,7 @@
 import Dexie from "dexie";
 
 export const db = new Dexie("ALAMAT_DB");
-db.version(4).stores({
-  questions: "++id, subject, topic, difficulty, seen",
-  progress: "topic, subject, correctAttempts, totalAttempts",
-});
-// v5: adds nextReviewAt for spaced repetition, intervalStep for SRS scheduling
 db.version(5).stores({
   questions: "++id, subject, topic, difficulty, seen, nextReviewAt",
   progress: "topic, subject, correctAttempts, totalAttempts",
-}).upgrade(() => {});
+});
